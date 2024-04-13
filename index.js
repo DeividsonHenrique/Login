4eb4b4414bc0924a74631f29aea8ae86a9ac625f
Deividson Henry <deividson.henrique2016@hotmail.com>
2024-04-13 19:27:40 -0300

    day 13.04.24

---
+++ index.js
@@ -0,0 +1,5 @@
+function esconderIcone(){document.getElementById('iconePrimeiro').style.display = 'none'}
+function mostarIcone(){document.getElementById('iconePrimeiro').style.display = 'block'}
+
+function esconderIconeS(){document.getElementById('iconeSegundo').style.display = 'none'}
+function mostarIconeS(){document.getElementById('iconeSegundo').style.display = 'block'}
